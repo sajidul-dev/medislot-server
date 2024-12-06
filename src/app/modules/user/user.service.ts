35092ec23ff49972db3dd5df0b4c6c10ef18e16f
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-12-07 01:14:33 +0600

    Husky check

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,6 +2,7 @@ import { IUser } from "./user.interface";
 import { User } from "./user.model";
 
 const getStudents = async (): Promise<IUser[] | null> => {
+  cons b=5
   const users = await User.find();
   return users;
 };
