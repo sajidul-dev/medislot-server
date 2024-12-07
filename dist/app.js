664f8a28179ea282e86950fcf41243139efd9ec4
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-12-07 17:11:34 +0600

    [Done]: Vercel script added for deployment

--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -10,14 +19,14 @@ const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.use(express_1.default.urlencoded({ extended: true }));
-app.post("/user", async (req, res) => {
+app.post("/user", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const new_user = {
         name: req.body.name,
         email: req.body.email,
     };
-    const result = await config_1.default.prisma.user.create({
+    const result = yield config_1.default.prisma.user.create({
         data: new_user,
     });
     res.send(result);
-});
+}));
 exports.default = app;
