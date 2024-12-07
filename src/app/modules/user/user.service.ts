d539bc122f36ae8dd268d06c9f3c66000cccae16
sajidul-dev <mdsajidulalam1245@gmail.com>
2024-12-07 15:48:58 +0600

    [Done]: Husky setup

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,8 +2,6 @@ import { IUser } from "./user.interface";
 import { User } from "./user.model";
 
 const getStudents = async (): Promise<IUser[] | null> => {
-  const b = 5;
-  const a = 4;
   const users = await User.find();
   return users;
 };
